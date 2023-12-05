b898cec601c499abecf842887b9839e22854cff3
Asya <135468933+axionyy@users.noreply.github.com>
2023-12-05 09:47:51 +0300

    initial commit

---
+++ app/src/main/java/com/example/pochti_gotovo/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.pochti_gotovo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void change(View view){
+        Intent intent = new Intent(this, MainAppActivity.class);
+        startActivity(intent);
+    }
+}
